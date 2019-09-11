a074782538a789a38e4a162984c748ed59df38d8
garbochung <g13what.never@gmail.com>
2019-09-11 15:32:04 -0700

    Garbo update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GTEST2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GTEST2.java
@@ -3,18 +3,64 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import java.lang.Math;
+
 
 @TeleOp
 public class GTEST2 extends LinearOpMode {
-    DcMotor motor;
+    DcMotor motor1, motor2;
+    Servo servoTest;
 
     public void runOpMode() throws InterruptedException {
 
-        motor = hardwareMap.dcMotor.get("motor");
+        motor1 = hardwareMap.dcMotor.get("motor1");
+        motor2 = hardwareMap.dcMotor.get("motor2");
+        servoTest = hardwareMap.servo.get("servo1");
+
         waitForStart();
+        boolean button = false;
         while (opModeIsActive()) {
-            motor.setPower(gamepad1.left_stick_y);
+            telemetry.addData("opModeIsActive", opModeIsActive());
+            telemetry.update();
+
+            int LeftD, RightD;
+            if (gamepad1.left_stick_y < 0) {
+                LeftD = -1;
+            } else {
+                LeftD = 1;
+            }
+
+            if (gamepad1.right_stick_y < 0) {
+                RightD = 1;
+            } else {
+                RightD = -1;
+            }
+
+
+            motor1.setPower(0.8*LeftD * Math.pow(Math.abs(gamepad1.left_stick_y),2));
+            telemetry.addData("motor1", LeftD * Math.sqrt(Math.abs(gamepad1.left_stick_y)));
+            motor2.setPower(0.8*RightD * Math.pow(Math.abs(gamepad1.right_stick_y),2));
+            telemetry.addData("motor2", RightD * Math.sqrt(Math.abs(gamepad1.right_stick_y)));
+            telemetry.update();
+
+
+            if (gamepad1.right_bumper && button) {
+                servoTest.setPosition(-0.2);
+                button = false;
+            } else if (gamepad1.left_bumper && !button) {
+                servoTest.setPosition(0.35);
+                button = true;
+            }
+            telemetry.addData("opModeIsActive", opModeIsActive());
+
+            telemetry.addData("Servo Position", servoTest.getPosition());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
         }
     }
 
 }
+
+
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GTestAuto.java
@@ -0,0 +1,160 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.List;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+/**
+ * This 2018-2019 OpMode illustrates the basics of using the TensorFlow Object Detection API to
+ * determine the position of the gold and silver minerals.
+ * <p>
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
+ * <p>
+ * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
+ * is explained below.
+ */
+@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
+public class GTestAuto extends LinearOpMode {
+    private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
+
+    /*
+     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+     * web site at https://developer.vuforia.com/license-manager.
+     *
+     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+     * random data. As an example, here is a example of a fragment of a valid key:
+     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+     * Once you've obtained a license key, copy the string from the Vuforia web site
+     * and paste it in to your code on the next line, between the double quotes.
+     */
+    private static final String VUFORIA_KEY = "AXzW9CD/////AAAAGTPAtr9HRUXZmowtd9p0AUwuXiBVONS/c5x1q8OvjMrQ8/XJGxEp0TP9Kl8PvqSzeXOWIvVa3AeB6MyAQboyW/Pgd/c4a4U/VBs1ouUsVBkEdbaq1iY7RR0cjYr3eLwEt6tmI37Ugbwrd5gmxYvOBQkGqzpbg2U2bVLycc5PkOixu7PqPqaINGZYSlvUzEMAenLOCxZFpsayuCPRbWz6Z9UJfLeAbfAPmmDYoKNXRFll8/jp5Ie7iAhSQgfFggWwyiqMRCFA3GPTsOJS4H1tSiGlMjVzbJnkusPKXfJ0dK3OH9u7ox9ESpi91T0MemXw3nn+/6QRvjGtgFH+wMDuQX7ta89+yW+wqdXX9ZQu8BzY";
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * {@link #tfod} is the variable we will use to store our instance of the Tensor Flow Object
+     * Detection engine.
+     */
+    private TFObjectDetector tfod;
+
+    @Override
+    public void runOpMode() {
+        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
+        // first.
+        initVuforia();
+
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTfod();
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
+        }
+
+        /** Wait for the game to begin */
+        telemetry.addData(">", "Press Play to start tracking");
+        telemetry.update();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            /** Activate Tensor Flow Object Detection. */
+            if (tfod != null) {
+                tfod.activate();
+            }
+
+            while (opModeIsActive()) {
+                telemetry.addData("Cube position", vuforiaRecognize());
+                telemetry.update();
+            }
+
+            if (tfod != null) {
+                tfod.shutdown();
+            }
+        }
+
+    }
+
+    public Integer vuforiaRecognize() {
+
+        Integer cubePosition = -1;
+
+        while (cubePosition == -1) {
+            if (tfod != null) {
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
+                    telemetry.addData("# Object Detected", updatedRecognitions.size());
+                    if (updatedRecognitions.size() == 2) {
+                        int goldMineralX = -1;
+                        int silverMineralX = -1;
+                        for (Recognition recognition : updatedRecognitions) {
+                            if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                goldMineralX = (int) recognition.getLeft();
+                            } else {
+                                silverMineralX = (int) recognition.getLeft();
+                            }
+                        }
+                        if (goldMineralX != -1 && silverMineralX != -1) {
+                            if (goldMineralX < silverMineralX ) {
+                                cubePosition = 0;
+                                //telemetry.addData("Gold Mineral Position", "Left");
+                            } else if (goldMineralX > silverMineralX) {
+                                cubePosition = 1;
+                                //telemetry.addData("Gold Mineral Position", "Right");
+                            }
+                        }
+                    }
+                    telemetry.update();
+                }
+            }
+        }
+
+        return cubePosition;
+    }
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the Tensor Flow Object Detection engine.
+    }
+
+    /**
+     * Initialize the Tensor Flow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
+    }
+}
+
